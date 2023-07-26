fca6606289c9ee007bd2f9f6a06494a2fa71f301
TakenHara <kavlod12345@gmail.com>
2023-07-26 14:28:38 +0200

    feat: add adaptive & decor elems

--- src/js/sprite.js
+++ src/js/sprite.js
@@ -1,5 +1,15 @@
-import camera from "../assets/sprite/icon-camera.svg";
+import boat from "../assets/sprite/icon-boat.svg";
+import delivery from "../assets/sprite/icon-delivery.svg";
+import person from "../assets/sprite/icon-person.svg";
+import production from "../assets/sprite/icon-production.svg";
+import warehouse from "../assets/sprite/icon-warehouse.svg";
+import loading from "../assets/sprite/icon-loading.svg";
 
 export default {
-    camera
+    boat,
+    delivery,
+    person,
+    production,
+    warehouse,
+    loading,
 };
